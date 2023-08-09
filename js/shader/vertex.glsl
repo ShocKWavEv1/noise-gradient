6f35ab33135b536eb06e814e672d7709a895dfc1
rodd.sal <rodrigosalmeron@tuhabi.mx>
2023-08-08 19:59:39 -0600

    rotate shader

--- js/shader/vertex.glsl
+++ js/shader/vertex.glsl
@@ -5,6 +5,12 @@ uniform vec2 pixels;
 float PI = 3.141592653589793238;
 void main() {
   vUv = uv;
-  vPosition = position;
-  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+  vec4 rotatedPosition = mat4(
+    cos(90.0), -sin(90.0), 0.0, 0.0,
+    sin(90.0), cos(90.0), 0.0, 0.0,
+    0.0, 0.0, 1.0, 0.0,
+    0.0, 0.0, 0.0, 1.0
+  ) * modelViewMatrix * vec4(position, 1.0);
+
+  gl_Position = projectionMatrix * rotatedPosition;
 }
